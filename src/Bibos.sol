56b4f8998bcf4f37b1be26fd7a66b96f6d358ab8
Nick Beattie <nick@nickbytes.com>
2022-04-02 15:59:47 -0700

    chore: better prettier formatting (#34)
    * chore: better prettier formatting
    
    chore: better prettier formatting
    
    chore: testing that it works w/ editor
    
    * chore: yolo runs prettier on all solidity files lfg

--- src/Bibos.sol
+++ src/Bibos.sol
@@ -1,30 +1,30 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.0;
 
-import { ERC721 } from 'solmate/tokens/ERC721.sol';
-import { Render } from './libraries/Render.sol';
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {Render} from "./libraries/Render.sol";
 
 contract Bibos is ERC721 {
-  address public owner;
-  uint256 public totalSupply;
+    address public owner;
+    uint256 public totalSupply;
 
-  mapping(uint256 => bytes32) public data;
+    mapping(uint256 => bytes32) public data;
 
-  constructor() ERC721('Bibos', 'BBO') {
-    owner = msg.sender;
-  }
+    constructor() ERC721("Bibos", "BBO") {
+        owner = msg.sender;
+    }
 
-  function tokenURI(uint256 id) public view override returns (string memory) {
-    require(id < totalSupply, 'invalid id');
-    return Render.tokenURI(id, data[id]);
-  }
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        require(id < totalSupply, "invalid id");
+        return Render.tokenURI(id, data[id]);
+    }
 
-  function mint() external payable {
-    uint256 id = totalSupply++;
+    function mint() external payable {
+        uint256 id = totalSupply++;
 
-    // derive random data
-    data[id] = keccak256(abi.encodePacked(msg.sender, block.timestamp));
-    // mint token
-    _mint(msg.sender, id);
-  }
+        // derive random data
+        data[id] = keccak256(abi.encodePacked(msg.sender, block.timestamp));
+        // mint token
+        _mint(msg.sender, id);
+    }
 }
