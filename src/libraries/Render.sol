4dbaa11d976df5a6814aef820d6b594751db6a83
g-a-v-i-n <gvnatknsn@gmail.com>
2022-04-06 15:34:21 -0400

    add preserveAspectRatio and correct width/height values on <svg>

--- src/libraries/Render.sol
+++ src/libraries/Render.sol
@@ -29,13 +29,13 @@ library Render {
         return
             string.concat(
                 "<svg ",
-                'pauseAnimations="true" '
                 'xmlns="http://www.w3.org/2000/svg" '
                 'xmlns:xlink="http://www.w3.org/1999/xlink" '
-                'width="500" '
-                'height="500" '
+                'width="100%" '
+                'height="100%" '
                 'viewBox="0 0 300 300" '
                 'fill="none" '
+                'preserveAspectRatio="xMidYMid meet" '
                 ">",
                 defs(),
                 svgChildren(data),
