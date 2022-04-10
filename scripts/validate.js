5117ae7a5ea6c09d4760d05146f330cf82357ae3
mike <mshrieve@gmail.com>
2022-04-10 16:13:23 -0700

    update: use decodeBase64

--- scripts/validate.js
+++ scripts/validate.js
@@ -8,7 +8,7 @@ const stripMimeType = (encoding) => encoding.slice(encoding.indexOf(",") + 1);
 
 const tokenUri = process.argv[2];
 const metadata = JSON.parse(decodeBase64(tokenUri));
-const svg = Buffer.from(stripMimeType(metadata.image), "base64").toString("ascii");
+const svg = decodeBase64(metadata.image);
 const validated = XMLValidator.validate(svg);
 
 // return 1 if invalid
