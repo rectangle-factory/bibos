b8b077908a848ab519c6dff7baa1bc1885e87aa4
mike <mshrieve@gmail.com>
2022-04-28 18:46:55 -0700

    add: fetch error handling

--- packages/inspector/hooks/useNft.tsx
+++ packages/inspector/hooks/useNft.tsx
@@ -22,16 +22,34 @@ const nftStateDefault: NFTState = {
   rawSvg: "",
 };
 
+// returns the entire tokenURI
+const RENDER_ENDPOINT = "/api/render";
+
 export const useNft = () => {
   const [state, setState] = useState(nftStateDefault);
 
   const handleFetchNFT = async () => {
     if (state.status == NFTStatus.FETCHING) return;
+
     setState((state) => ({ ...state, status: NFTStatus.FETCHING }));
-    const response = await fetch("/api/render");
+
+    const response = await fetch(RENDER_ENDPOINT);
+
+    // handle fetch error
+    if (response.status != 200) {
+      console.log("fetch error: ", response.status);
+      return setState(nftStateDefault);
+    }
+
     const text = await response.text();
+
+    // decode and parse metadata
     const metadata = JSON.parse(decodeBase64(text));
+
+    // random token id
     const tokenId = Math.floor(Math.random() * 999);
+
+    // decode svg
     const rawSvg = decodeBase64(metadata.image);
     setState({ status: NFTStatus.FETCHED, metadata, tokenId, rawSvg });
   };
--- packages/inspector/pages/api/render.ts
+++ packages/inspector/pages/api/render.ts
@@ -6,15 +6,16 @@ const { exec } = require("child_process");
 export default async (request: NextApiRequest, response: NextApiResponse) =>
   new Promise<void>((resolve, reject) =>
     exec("cd ../.. && ./scripts/get_token_uri.sh", (err, stdout, stderr) => {
+      console.log("err: ", err);
       if (stderr) {
-        console.error(`exec error: ${stderr}`);
-        response.json(stderr);
-        response.status(405).end();
-        reject();
+        console.log("err: ", err);
+        console.log("stderr: ", stderr);
+        response.status(500);
+        response.send(stderr);
+        return resolve();
       }
       console.log("synthesis_engine: synthesized bibos");
       response.send(stdout);
-      response.end();
       resolve();
     })
   );
