22f5bcc6e7f13528679899bee92de1a15ae61c8a
mike <mshrieve@gmail.com>
2022-04-28 19:15:15 -0700

    update: get_token_uri.sol

--- src/scripts/get_token_uri.sol
+++ src/scripts/get_token_uri.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.0;
 
-import {vm} from "../util/vm.sol";
-import {io} from "../util/io.sol";
-import {time} from "../util/time.sol";
-
-import {Bibos} from "../Bibos.sol";
 import {Test, console2 as console} from "forge-std/Test.sol";
 
-contract get_token_uri is Test {
-    string constant TOKEN_URI_OUTPUT_PATH = "./output/token_uri.base64";
+import {Bibos} from "../Bibos.sol";
+import {time} from "../util/time.sol";
 
+contract get_token_uri is Test {
     function run() external {
         Bibos bibos = new Bibos();
+
         // get current time to use as random seed
         uint256 unixTime = time.setToUnixTime();
 
