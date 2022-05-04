0498250e515da354a810ee8569937b51a5e4fdf4
g-a-v-i-n <gvnatknsn@gmail.com>
2022-05-03 19:16:07 -0700

    remove animation property from svg tag

--- src/libraries/SVG.sol
+++ src/libraries/SVG.sol
@@ -8,7 +8,6 @@ library SVG {
         return
             string.concat(
                 "<svg ",
-                'pauseAnimations="true" '
                 'xmlns="http://www.w3.org/2000/svg" '
                 'xmlns:xlink="http://www.w3.org/1999/xlink" '
                 'width="100%" '
