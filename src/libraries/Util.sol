7370a24dbf7b203f0961c4090511e2ebe02cac54
mike <mshrieve@gmail.com>
2022-06-13 01:50:18 -0700

    fix typo

--- src/libraries/Util.sol
+++ src/libraries/Util.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity >=0.8.0;
-import {Test, console2} from "forge-std/test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 
 /// @title the bibos utility library
 /// @notice utility functions
