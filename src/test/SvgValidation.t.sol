e70c8bb73e143534845d7f62366b5e07aab0276b
mike <mshrieve@gmail.com>
2022-06-21 20:16:10 -0700

    small updates

--- src/test/SvgValidation.t.sol
+++ src/test/SvgValidation.t.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.0;
 
 import {Test, console2 as console} from "forge-std/Test.sol";
 import {Bibos} from "../Bibos.sol";
-import {vm} from "../util/vm.sol";
 import {time} from "../util/time.sol";
 
 contract SvgValidationTest is Test {
