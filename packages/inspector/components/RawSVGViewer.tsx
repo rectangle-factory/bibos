52fbd95a073d8b713f8cf465ae976ae75b18127b
g-a-v-i-n <gvnatknsn@gmail.com>
2022-07-03 13:39:01 -0400

    feat: download buttons for svg and png

--- packages/inspector/components/RawSVGViewer.tsx
+++ packages/inspector/components/RawSVGViewer.tsx
@@ -1,20 +1,63 @@
+import { useEffect, useRef } from "react";
+import { downloadSVG, downloadPNG, scaleCanvas } from "../util/index";
+
 type RawSVGViewerProps = {
   debug?: boolean;
   className?: string;
   rawSVG: string;
   isLoading?: boolean;
+  tokenId: number;
 };
 
+const WIDTH = 1200;
+const HEIGHT = 1200;
+
 export function RawSVGViewer(props: RawSVGViewerProps) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    scaleCanvas(canvasRef);
+  }, []);
+
   return (
     <div
       className={`rounded-3xl shadow-elevation-medium relative h-full w-auto bg-white/5 ${props.className}`}
     >
+      <canvas
+        ref={canvasRef}
+        width={WIDTH}
+        height={HEIGHT}
+        className="fixed top-[-10000px] left-[-10000px] invisible"
+      />
+
       <div
-        className={`rounded-3xl h-full w-auto overflow-hidden transition-opacity duration-300 ${props.isLoading ? 'opacity-0' : 'opacity-100'} ${props.debug ? "debug-svg" : ""}`}
+        className={`rounded-3xl h-full w-auto overflow-hidden transition-opacity duration-300 ${
+          props.isLoading ? "opacity-0" : "opacity-100"
+        } ${props.debug ? "debug-svg" : ""}`}
         dangerouslySetInnerHTML={{ __html: props.rawSVG }}
       />
-      <div className={`rounded-3xl z-90 absolute content-none top-0 left-0 right-0 bottom-0 ${props.debug ? 'inner-shadow-border-debug':"inner-shadow-border"}`} />
+      <div
+        className={`rounded-3xl z-90 absolute content-none top-0 left-0 right-0 bottom-0 ${
+          props.debug ? "inner-shadow-border-debug" : "inner-shadow-border"
+        }`}
+      />
+      <div className="absolute top-0 left-0 right-0 bottom-0 rounded-3xl opacity-0 hover:opacity-100">
+        <div className="absolute right-0 bottom-0 p-4 flex gap-x-2">
+          <button
+            className="text-sm exclusion bg-white/10 border border-white/10 rounded-lg px-2 py-0.5 text-white"
+            onClick={() => downloadSVG(props.rawSVG, 'bibo' + props.tokenId)}
+          >
+            ↓ SVG
+          </button>
+
+          <button
+            className="text-sm exclusion bg-white/10 border border-white/10 rounded-lg px-2 py-0.5 text-white"
+            onClick={() => downloadPNG(canvasRef, props.rawSVG, 'bibo' + props.tokenId, WIDTH, HEIGHT)}
+          >
+            ↓ PNG
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- packages/inspector/components/ToggleButton.tsx
+++ packages/inspector/components/ToggleButton.tsx
@@ -7,7 +7,11 @@ export function ToggleButton(props: ButtonProps & { isOn: boolean }) {
       className="px-2 py-0.5 bg-white/20 border border-white/5 text-white rounded-lg flex items-center justify-center gap-x-1.5 shadow-elevation-low "
       onClick={props.onClick}
     >
-      <div className={`transition-all user-select-none w-2 h-2 rounded-full ${props.isOn ? "bg-white shadow-glow" : "bg-white/20 shadow-none"}`} />
+      <div
+        className={`transition-all user-select-none w-2 h-2 rounded-full ${
+          props.isOn ? "bg-white shadow-glow" : "bg-white/20 shadow-none"
+        }`}
+      />
       {props.children}
     </button>
   );
