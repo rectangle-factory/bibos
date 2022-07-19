c5312ceb94b3f716ce0a736366c0232aed249c3c
mike <mshrieve@gmail.com>
2022-07-19 11:48:03 -0700

    add multirender api route and script

---
+++ scripts/multi_render.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Notice: run from root directory.
+
+extract_json_output() {
+    echo "$1" | grep "{" 
+}
+
+# LOCAL_RENDER_SCRIPT_PATH="src/scripts/local_render.sol"
+SCRIPT_OUTPUT="$(forge script local_render --json -s "run(uint256)" $1)"
+
+JSON_OUTPUT="$(extract_json_output "$SCRIPT_OUTPUT")"
+
+
+echo "$JSON_OUTPUT" | jq .returns.tokenURIs.value
