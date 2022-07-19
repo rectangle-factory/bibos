c4bc3e11ac81734a0150a211069ff2eef45f7552
mike <mshrieve@gmail.com>
2022-07-19 15:45:42 -0700

    add error handling back to api routes

--- packages/inspector/pages/api/render.ts
+++ packages/inspector/pages/api/render.ts
@@ -8,12 +8,11 @@ export default async (request: NextApiRequest, response: NextApiResponse) =>
   new Promise<void>((resolve, _) =>
     exec("cd ../.. && " + LOCAL_RENDER_SCRIPT_PATH, (_: any, stdout: string, stderr: string) => {
       if (stderr) {
-        // log the error to console
         console.log("stderr: ", stderr);
-        // ignore errors until solmate updates their foundry.toml
-        // response.status(500);
-        // response.send(stderr);
-        // return resolve();
+
+        response.status(500);
+        response.send(stderr);
+        return resolve();
       }
 
       response.json(JSON.parse(stdout));
