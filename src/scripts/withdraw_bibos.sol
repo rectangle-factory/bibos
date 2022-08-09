4a3e00ca33550efacaff4a1e2324bc8dee870537
mike <mshrieve@gmail.com>
2022-08-09 15:37:30 -0700

    remove vault address

--- src/scripts/withdraw_bibos.sol
+++ src/scripts/withdraw_bibos.sol
@@ -6,12 +6,11 @@ import {Bibos} from "src/Bibos.sol";
 
 contract withdraw_bibos is Script {
     address constant bibosAddress = 0xF528e3381372c43F5e8a55b3E6c252E32F1a26e4;
-    address payable constant vault = payable(0x972011A6F6a526E798F7D459502f686FA13Da88f);
 
-    function run() public returns (address, address) {
+    function run(address payable _to) public returns (address, address) {
         vm.startBroadcast();
         Bibos bibos = Bibos(0xF528e3381372c43F5e8a55b3E6c252E32F1a26e4);
-        bibos.withdraw(vault);
+        bibos.withdraw(_to);
         return (msg.sender, bibos.owner());
     }
 }
